61fb694041f1acee60bb77b9cf219d8ba1d0fb65
Jim O'Regan <joregan@kth.se>
2022-11-09 14:30:54 +0100

    complete script (fingers crossed)

--- sync_asr/kaldi/index_riksdag_api.py
+++ sync_asr/kaldi/index_riksdag_api.py
@@ -1,8 +1,23 @@
 import json
 from bs4 import BeautifulSoup
+from pathlib import Path
+import argparse
 
 
 BASE_KEYS = ['videostatus', 'committee', 'type', 'debatepreamble', 'debatetexthtml', 'livestreamurl', 'activelivespeaker', 'id', 'dokid', 'title', 'debatename', 'debatedate', 'debatetype', 'debateurl', 'fromchamber', 'thumbnailurl', 'debateseconds']
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="""
+    Convert a directory of Riksdag API output to Kaldi text format
+    """)
+    parser.add_argument('dir', type=str, help='directory containing API JSON')
+    parser.add_argument('output', type=str, help='output file name')
+    args = parser.parse_args()
+
+    return args
+
+
 def read_api_json(filename):
     infile = str(filename)
     with open(infile) as input:
@@ -52,3 +67,36 @@ def read_api_json(filename):
             speakers.append(pg)
             count += 1
     return base, speakers
+
+
+def clean_text(text):
+    text = text.replace("\r\n", " ")
+    text = text.replace("\n", " ")
+    text = text.strip()
+    text = text.replace(". ", " ")
+    text = text.replace(",", " ")
+    text = text.replace(";", "")
+    text = text.replace(":", "")
+    text = text.replace("!", "")
+    text = text.replace("?", "")
+    text = text.lower()
+    
+    return text
+
+
+def main():
+    args = get_args()
+    API_OUTPUT = Path(args.dir)
+    with open(args.output, "w") as outf:
+        for file in API_OUTPUT.glob("*"):
+            doc, speakers = read_api_json(file)
+            if doc is None or speakers is None:
+                continue
+            for speaker in speakers:
+                docid = f'{doc["streamurl"]}_{speaker["paragraph"]}'
+                text = clean_text(speaker["text"])
+                outf.write(f"{docid} {text}\n")
+
+
+if __name__ == '__main__':
+    main()
