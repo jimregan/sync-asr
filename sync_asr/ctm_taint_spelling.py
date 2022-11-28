99e93f5c132e3bd48262943b15f9300ddddc4d1d
Jim O'Regan <joregan@kth.se>
2022-11-28 12:54:05 +0100

    finish basic version of spell checking script

---
+++ sync_asr/ctm_taint_spelling.py
@@ -0,0 +1,79 @@
+try:
+    import hunspell
+except ImportError:
+    print("Could not import hunspell")
+    print("Hint: pip install hunspell")
+    if __name__ == '__main__':
+        quit()
+    class HunspellChecker():
+        pass
+
+import argparse
+from .ctm_edit import CTMEditLine
+
+
+class HunspellChecker():
+    def __init__(self, dict, aff):
+        self.dict = dict
+        self.aff = aff
+        self.speller = hunspell.HunSpell(dict, aff)
+
+    def check(self, text):
+        return self.speller.spell(text)
+
+    def check_pair(self, text, ref):
+        if self.speller.spell(text) and not self.speller.spell(ref):
+            return "correct_text"
+        elif self.speller.spell(ref) and not self.speller.spell(text):
+            return "correct_ref"
+        elif self.speller.spell(ref) and self.speller.spell(text):
+            return "correct_both"
+        else:
+            return "incorrect_both"
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        """Checks a CTMEdit file for spelling errors""")
+
+    parser.add_argument("--dict-path",
+                        type=str,
+                        default="/usr/share/hunspell/sv_SE.dic",
+                        help="""Path to the Hunspell dictionary to use""")
+    parser.add_argument("--aff-path",
+                        type=str,
+                        default="/usr/share/hunspell/sv_SE.aff",
+                        help="""Path to the Hunspell affix file to use""")
+    parser.add_argument("ctm_edits_in",
+                        type=argparse.FileType('r'),
+                        help="""Filename of input ctm-edits file.  Use
+                        /dev/stdin for standard input.""")
+    args = parser.parse_args()
+    return args
+
+
+def inline_check_unigram(ctm_lines, speller):
+    for line in ctm_lines:
+        if line.edit == "cor":
+            if speller.spell(line.text):
+                continue
+            else:
+                line.set_prop("spelling", "incorrect_both")
+        else:
+            prop = speller.check_pair(line.text, line.ref)
+            line.set_prop("spelling", prop)
+
+
+def main():
+    args = get_args()
+    speller = HunspellChecker(dict=args.dict_path, aff=args.aff_path)
+    ctm_lines = []
+    for line in args.ctm_edits_in.readlines():
+        ctm_lines.append(CTMEditLine(line))
+    inline_check_unigram(ctm_lines, speller)
+    for item in ctm_lines:
+        print(item)    
+
+
+if __name__ == '__main__':
+    main()
