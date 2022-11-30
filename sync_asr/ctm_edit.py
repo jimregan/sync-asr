5bfcddd54ac1bbdf45a6cc9b74d9b3573caa9399
Jim O'Regan <joregan@kth.se>
2022-11-30 22:57:24 +0100

    semicolon is not necessarily present

--- sync_asr/ctm_edit.py
+++ sync_asr/ctm_edit.py
@@ -38,7 +38,7 @@ class CTMEditLine(TimedWord):
             if parts[8] == "tainted":
                 self.tainted = True
         # Extension to the format
-        if len(parts) > 8 and (":" in parts[-1] and ";" in parts[-1]):
+        if len(parts) > 8 and ":" in parts[-1]:
             self.props = { k:v for k,v in (p.split(":") for p in parts[-1].split(";")) }
 
     def as_list(self):
