206ba817a7d4502d852f08534eb592efe23bca2f
Jim O'Regan <joregan@kth.se>
2022-11-30 14:47:41 +0100

    refactor

--- sync_asr/kaldi/hfjson_to_ctm.py
+++ sync_asr/kaldi/hfjson_to_ctm.py
@@ -15,8 +15,7 @@ def get_args():
 
 
 def read_hfjson(json_file, lowercase=True, riksdag=False):
-    outname = json_file.replace(".json", ".ctm")
-    with open(json_file) as jsonf, open(outname, "w") as outf:
+    with open(json_file) as jsonf:
         utt = json_file.split("/")[-1].replace(".json", "")
         if riksdag:
             utt = utt.replace("_480p", "")
@@ -24,20 +23,28 @@ def read_hfjson(json_file, lowercase=True, riksdag=False):
         if not "chunks" in data:
             raise ValueError(f"File does not appear to contain HuggingFace JSON")
         # utt_id channel_num start_time duration phone_id confidence
-        count = 1
+        ctm_lines = []
         for chunk in data["chunks"]:
-            # do stuff
             word = chunk["text"] if not lowercase else chunk["text"].lower()
             start = chunk["timestamp"][0]
             dur = chunk["timestamp"][1] - chunk["timestamp"][0]
-            outf.write(f"{utt} 1 {start} {dur} {word} 1.0\n")
+            ctm_lines.append(f"{utt} 1 {start} {dur} {word} 1.0")
+        return ctm_lines
+
+
+def read_hfjson_write_ctm(json_file, lowercase=True, riksdag=False):
+    ctm_lines = read_hfjson(json_file, lowercase, riksdag)
+    outname = json_file.replace(".json", ".ctm")
+    with open(outname, "w") as outf:
+        for line in ctm_lines:
+            outf.write(f"{line}\n")
 
 
 def main():
     args = get_args()
     lc = args.lower
     for file in args.files:
-        read_hfjson(file, lc)
+        read_hfjson_write_ctm(file, lc)
 
 
 if __name__ == '__main__':
