68b50ab5bf3fd3e6fe1d579e25b5329282b39a94
Jim O’Regan <joregan@kth.se>
2022-11-30 20:51:20 +0100

    Tidy main (#16)

--- sync_asr/hf_json_input.py
+++ sync_asr/hf_json_input.py
@@ -1,27 +1,32 @@
-from .elements import TimedElement, TimedWord
-from typing import List
+from .elements import TimedWordSentence, TimedWord
 import json
 
 
-class HuggingFaceJSON(TimedElement):
-    words: List[TimedWord]
-
+class HuggingFaceJSON(TimedWordSentence):
     def __init__(self, data=None, filename=""):
-        self.foo = "foo"
-        if data is None and filename != "":
-            self._load(filename)
+        if data is None:
+            words = self._load(filename)
+        elif filename == "":
+            words = self._grab(data)
+        super().__init__(words)
 
     def _load(self, filename):
         with open(filename) as jsonf:
             data = json.load(jsonf)
             if not "chunks" in data:
                 raise ValueError(f"File {filename} does not appear to contain HuggingFace JSON")
-            self._grab(data, False)
+            return self._grab(data, False)
 
-    def _grab(self, data, warn=True):
-        if warn and not "chunks" in data:
+    def _grab(self, data, warn=False):
+        words = []
+        if type(data) == str:
+            data = json.loads(data)
+        if not "chunks" in data or type(data["chunks"]) != list:
             raise ValueError(f"Data does not appear to contain HuggingFace JSON")
         for chunk in data["chunks"]:
-            self.words.append(TimedWord(start_time=chunk["timestamp"][0],
-                                        end_time=chunk["timestamp"][1]),
-                                        text=chunk["text"])
+            if warn:
+                print(f'Reading chunk: {chunk["timestamp"][0]}:{chunk["timestamp"][1]} {chunk["text"]}')
+            words.append(TimedWord(start_time=int(chunk["timestamp"][0] * 1000),
+                                        end_time=int(chunk["timestamp"][1] * 1000),
+                                        text=chunk["text"]))
+        return words
--- sync_asr/sentence_timed_input.py
+++
@@ -1,5 +0,0 @@
-from .input_source import InputSource
-
-class SentenceTimedInputSource(InputSource):
-    def __repr__(self) -> str:
-        return super().__repr__()
