0af8d095cbfbd2f6aea06c1d08fc33f23307ac2d
Jim O'Regan <joregan@kth.se>
2022-11-30 20:17:15 +0100

    call super()

--- sync_asr/hf_json_input.py
+++ sync_asr/hf_json_input.py
@@ -5,21 +5,21 @@ import json
 
 class HuggingFaceJSON(TimedElement):
     def __init__(self, data=None, filename=""):
-        self.words = []
-        if data is None and filename != "":
-            self._load(filename)
+        if data is None:
+            words = self._load(filename)
         elif filename == "":
-            self._grab(data)
-        super().__init__(self.words)
+            words = self._grab(data)
+        super().__init__(words)
 
     def _load(self, filename):
         with open(filename) as jsonf:
             data = json.load(jsonf)
             if not "chunks" in data:
                 raise ValueError(f"File {filename} does not appear to contain HuggingFace JSON")
-            self._grab(data, False)
+            return self._grab(data, False)
 
     def _grab(self, data, warn=False):
+        words = []
         if type(data) == str:
             data = json.loads(data)
         if not "chunks" in data or type(data["chunks"]) != list:
@@ -27,6 +27,7 @@ class HuggingFaceJSON(TimedElement):
         for chunk in data["chunks"]:
             if warn:
                 print(f'Reading chunk: {chunk["timestamp"][0]}:{chunk["timestamp"][1]} {chunk["text"]}')
-            self.words.append(TimedWord(start_time=int(chunk["timestamp"][0] * 1000),
+            words.append(TimedWord(start_time=int(chunk["timestamp"][0] * 1000),
                                         end_time=int(chunk["timestamp"][1] * 1000),
                                         text=chunk["text"]))
+        return words
