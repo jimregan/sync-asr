cfd4a3faf516772445756b7c898ca2a396e3deb4
Jim O'Regan <joregan@kth.se>
2022-11-30 22:51:15 +0100

    flesh out test

--- tests/test_ctm_edit.py
+++ tests/test_ctm_edit.py
@@ -12,9 +12,12 @@ def test_ctmline():
     assert ctm_line.duration == 90
     assert ctm_line.text == "<eps>"
     assert ctm_line.confidence == 1.0
+    assert ctm_line.ref == "<eps>"
+    assert ctm_line.edit == "sil"
+    assert ctm_line.tainted == True
 
 
 def test_as_list():
     ctm_line = CTMEditLine(_SAMPLE)
-    #exp = ["AJJacobs_2007P-0001605-0003029", "1", "0.0", "0.09", "<eps>", "1.0"]
-    #assert ctm_line.as_list() == exp
+    exp = ["AJJacobs_2007P-0001605-0003029", "1", "0.0", "0.09", "<eps>", "1.0", "<eps>", "sil", "tainted"]
+    assert ctm_line.as_list() == exp
