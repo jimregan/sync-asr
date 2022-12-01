1b0a88e2fbd5d03db1026bdd1e631d30ce82f138
Jim O'Regan <joregan@kth.se>
2022-12-01 14:04:26 +0100

    check for streamurls

--- tests/test_riksdag_api.py
+++ tests/test_riksdag_api.py
@@ -8,3 +8,4 @@ TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 def test_riksdag_api():
     file = f"{TEST_DIR}/H001CU21"
     rdapi = RiksdagAPI(filename=file)
+    assert "streamurls" in rdapi.__dict__
