40a76752a41b664d69e0318ae75bebead6bc22d8
Jim O'Regan <joregan@kth.se>
2022-12-01 13:57:15 +0100

    basic test

---
+++ tests/test_riksdag_api.py
@@ -0,0 +1,10 @@
+import os
+from sync_asr.riksdag.riksdag_api import RiksdagAPI
+
+
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def test_riksdag_api():
+    file = f"{TEST_DIR}/H001CU21"
+    rdapi = RiksdagAPI(file)
