1ec0ad09d2c5901a55c690951c4f3e2d78df2ecf
Jim O'Regan <joregan@kth.se>
2022-12-01 14:25:43 +0100

    start test for SpeakerElement

--- tests/test_riksdag_api.py
+++ tests/test_riksdag_api.py
@@ -1,5 +1,5 @@
 import os
-from sync_asr.riksdag.riksdag_api import RiksdagAPI
+from sync_asr.riksdag.riksdag_api import RiksdagAPI, SpeakerElement
 
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -10,3 +10,14 @@ def test_riksdag_api():
     rdapi = RiksdagAPI(filename=file)
     assert "videodata" in rdapi.__dict__
     assert "streamurl" in rdapi.videodata
+
+
+def test_speaker_element():
+    file = f"{TEST_DIR}/H001CU21"
+    rdapi = RiksdagAPI(filename=file)
+    speakers = rdapi.videodata["speakers"]
+    se = SpeakerElement(speakers[0])
+    assert se.start_time == 14000
+    assert se.duration == 233000
+    assert se.speaker_name == "Ola Johansson"
+    assert se.text.startswith("Herr talman! Det blir lite")
