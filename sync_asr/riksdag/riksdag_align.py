6496f144fc98d9b8675587d189e2c94d69347287
Jim O'Regan <joregan@kth.se>
2022-12-06 16:42:32 +0100

    sketch

--- sync_asr/riksdag/riksdag_align.py
+++ sync_asr/riksdag/riksdag_align.py
@@ -3,12 +3,14 @@ from ..ctm import CTMLine
 from typing import List
 from dataclasses import dataclass
 from copy import deepcopy
+from ..kaldi.align_ctm_ref import get_ctm_edits, smith_waterman_alignment
+import string
 
 
 @dataclass
 class FilteredPair():
     ctmlines: List[CTMLine]
-    riksdag_segments: List[SpeakerElement]
+    riksdag_segments: SpeakerElement
     speaker_name: str = ""
 
 
@@ -20,7 +22,6 @@ def filter_ctm_with_riksdag(ctmlines, riksdag_output):
 
     last_i = i = j = 0
     pairs = []
-
 
     while (i < len(ctmlines) - 1) and (j < len(riksdag_output)):
         if (start or not within) and ctmlines[i].end_time < riksdag_output[j].start_time:
@@ -48,3 +49,32 @@ def filter_ctm_with_riksdag(ctmlines, riksdag_output):
     return pairs
 
 
+def default_similarity_score_function(x, y):
+    if x == y:
+        return 1
+    return -1
+
+
+def rd_similarity_score_function(x, y):
+    left = x
+    right = y.lower()
+    while right[-1] in string.punctuation:
+        right = right[:-1]
+
+    if left == right:
+        return 1
+    return -1
+
+
+def align_ctm_with_riksdag(pairs: List[FilteredPair],
+                           similarity_score_function=None,
+                           del_score=1, ins_score=1,
+                           eps_symbol="<eps>", align_full_hyp=True):
+    aligned_pairs = []
+
+    for pair in pairs:
+        if pair.riksdag_segments is not None:
+            left_text = " ".join([t.text for t in pair.ctmlines])
+            right_text = pair.riksdag_segments.text
+
+    pass
