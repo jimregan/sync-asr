ab77c1538b41b87a042e2838dc2b52a430268058
Jim O'Regan <joregan@kth.se>
2022-12-07 18:49:55 +0100

    this is gonna fail

--- tests/test_ctm_edit.py
+++ tests/test_ctm_edit.py
@@ -1,9 +1,19 @@
-from sync_asr.ctm_edit import CTMEditLine
+from sync_asr.ctm_edit import CTMEditLine, shift_epsilons
 
 
 _SAMPLE = "AJJacobs_2007P-0001605-0003029 1 0 0.09 <eps> 1.0 <eps> sil tainted"
 _SAMPLE2 = "AJJacobs_2007P-0001605-0003029 1 0 0.09 <eps> 1.0 <eps> sil tainted spelling:both"
 _SAMPLE3 = "AJJacobs_2007P-0001605-0003029 1 0 0.09 <eps> 1.0 <eps> sil spelling:both"
+_SAMPLE4 = """
+AJJacobs_2007P-0001605-0003029 1 0 0.09 foo 1.0 <eps> ins
+AJJacobs_2007P-0001605-0003029 1 0.1 0.09 bar 1.0 <eps> ins
+AJJacobs_2007P-0001605-0003029 1 0.2 0.09 bar 1.0 foo sub
+"""
+_EXP4 = """
+AJJacobs_2007P-0001605-0003029 1 0 0.09 foo 1.0 foo cor
+AJJacobs_2007P-0001605-0003029 1 0.1 0.09 bar 1.0 <eps> ins
+AJJacobs_2007P-0001605-0003029 1 0.2 0.09 bar 1.0 <eps> ins
+"""
 
 
 def test_ctmline():
@@ -37,3 +47,10 @@ def test_as_list():
     ctm_line = CTMEditLine(_SAMPLE)
     exp = ["AJJacobs_2007P-0001605-0003029", "1", "0.0", "0.09", "<eps>", "1.0", "<eps>", "sil", "tainted"]
     assert ctm_line.as_list() == exp
+
+
+def test_shift_epsilons():
+    ctmlines4 = [CTMEditLine(x) for x in _SAMPLE4.split("\n") if x != ""]
+    explines4 = [CTMEditLine(x) for x in _EXP4.split("\n") if x != ""]
+    ctmout = shift_epsilons(ctmout, comparison=None, forward=False, ref=True)
+    assert explines4 == ctmout
