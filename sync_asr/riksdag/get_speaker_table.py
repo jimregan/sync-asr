240a3caaa5378068d81faeffeef6b16e0f13b602
Jim O'Regan <joregan@kth.se>
2023-01-23 13:35:45 +0100

    Revert "nor this"
    This reverts commit 953e5d6a9bc47bb0d7f3c54a44132086220bacbb.

--- sync_asr/riksdag/get_speaker_table.py
+++ sync_asr/riksdag/get_speaker_table.py
@@ -275,7 +275,7 @@ def merge_year_ranges(ranges: List[YearRange]) -> List[YearRange]:
         assert type(cur) == YearRange, cur_err
         assert type(other) == YearRange, other_err
         if other.contains(cur):
-            cur = YearRange(other.from_text, other.to_text)
+            cur = other
             i += 1
         elif cur.contains(other):
             i += 1
