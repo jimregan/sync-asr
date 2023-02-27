f2a0fc4e21dd1af575944559e7247bebc5bbcfe8
Jim O'Regan <joregan@kth.se>
2023-02-27 14:42:27 +0100

    commit to revert

--- sync_asr/kaldi/segment_ctm_edits_mild.py
+++ sync_asr/kaldi/segment_ctm_edits_mild.py
@@ -45,7 +45,7 @@ def non_scored_words():
     return _global_non_scored_words
 
 
-def get_args():
+def get_args(argl=[]):
     parser = argparse.ArgumentParser(
         description="""This program produces segmentation and text information
         based on reading ctm-edits input format which is produced by
@@ -233,7 +233,10 @@ def get_args():
     parser.add_argument("--verbose", type=int, default=0,
                         help="Use higher verbosity for more debugging output")
 
-    args = parser.parse_args()
+    if argl == []:
+        args = parser.parse_args()
+    else:
+        args = parser.parse_args(argl)
 
     if args.verbose > 2:
         _global_handler.setLevel(logging.DEBUG)
