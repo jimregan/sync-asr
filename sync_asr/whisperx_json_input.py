5f47eff88ac52a3b785c4ef2d06b3422d2a41625
Jim O’Regan <joregan@kth.se>
2026-03-28 18:06:49 +0100

    Changes for mm-conv experiments (#58)

---
+++ sync_asr/whisperx_json_input.py
@@ -0,0 +1,57 @@
+# Copyright (c) 2022, 2024 Jim O'Regan for Språkbanken Tal
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from .elements import TimedWordSentence, TimedWord
+from pathlib import Path
+import json
+
+
+class WhisperXWord(TimedWord):
+    def __init__(self, start_time=0, end_time=0, text="", speaker=None):
+        super().__init__(start_time, end_time, text)
+        if speaker:
+            self.speaker = speaker
+
+
+class WhisperXJSON(TimedWordSentence):
+    def __init__(self, data=None, filename=""):
+        if data is None and filename != "":
+            words = self._load(filename)
+            fileid = Path(filename).stem
+        elif data is not None and filename == "":
+            words = self._grab(data)
+            fileid = None
+        else:
+            raise ValueError("Specify exactly one of 'data' or 'filename'")
+        super().__init__(words, fileid=fileid)
+
+    def _load(self, filename):
+        with open(filename) as jsonf:
+            data = json.load(jsonf)
+            if not "chunks" in data:
+                raise ValueError(f"File {filename} does not appear to contain WhisperX JSON")
+            return self._grab(data, False)
+
+    def _grab(self, data, warn=False):
+        words = []
+        if type(data) == str:
+            data = json.loads(data)
+        if not "chunks" in data or type(data["chunks"]) != list:
+            raise ValueError(f"Data does not appear to contain WhisperX JSON")
+        for chunk in data["chunks"]:
+            if warn:
+                print(f'Reading chunk: {chunk["timestamp"][0]}:{chunk["timestamp"][1]} {chunk["text"]}')
+            words.append(TimedWord(start_time=int(chunk["timestamp"][0] * 1000),
+                                        end_time=int(chunk["timestamp"][1] * 1000),
+                                        text=chunk["text"]))
+        return words
