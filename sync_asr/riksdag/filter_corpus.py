6fee84bf3a78f419cd9ac9722e1eb27d5b75bb9d
Jim O'Regan <joregan@kth.se>
2026-03-28 18:42:43 +0100

    update year

--- sync_asr/riksdag/filter_corpus.py
+++ sync_asr/riksdag/filter_corpus.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024 Jim O'Regan for Språkbanken Tal
+# Copyright (c) 2026 Jim O'Regan for Språkbanken Tal
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
